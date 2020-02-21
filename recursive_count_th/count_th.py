1d40da8c853d3d3d062ba745f6542b04a575999a
Tyler Berrett <btylerberrett@gmail.com>
2020-02-21 09:45:19 -0700

    add count_th function which takes in a string and recursively checks to see if the string contains any "th"'s in it

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,15 @@
-'''
+"""
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
-'''
+"""
+# search two letters at a time the remove one the keep track of how many th I find when returning
+# return if length of less then one
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    if len(word) <= 1:
+        return 0
+    search = word[:2]
+    new_word = word[1:]
+    if search == 'th':
+        return 1 + count_th(new_word)
+    return count_th(new_word)
